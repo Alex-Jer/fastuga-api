fe4869ca6845a562aebdc6ab4fbb364f00496cba
Alexandre <alexandrejeronimo2001@gmail.com>
2022-11-14 23:39:22 +0000

    resources

--- app/Http/Controllers/api/CustomerController.php
+++ app/Http/Controllers/api/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CustomerResource;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 
@@ -15,7 +16,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        return CustomerResource::collection(Customer::all());
     }
 
     /**
