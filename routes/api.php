86663ed454e3a2c0ed5d42d0d2d59af38e9da7e6
tgn <trwygon@outlook.pt>
2022-12-13 18:50:54 +0000

    middleware fix

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
-    Route::delete('logout', 'logout');
+    Route::delete('logout', 'logout')->middleware('auth:api');
 });
 
 Route::middleware('auth:api')->group(function () {
