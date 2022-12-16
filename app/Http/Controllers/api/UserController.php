3f2d92c5955096ac4da75ecdb95608b9da4ecd8b
tgn <trwygon@outlook.pt>
2022-12-16 18:16:52 +0000

    Edit users' accounts

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\UserPostRequest;
+use App\Http\Requests\User\UserPutRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Storage;
 
 class UserController extends Controller
 {
@@ -41,5 +43,24 @@ class UserController extends Controller
         unset($newUser['photo']);
 
         return response(["message" => "User created", "user" => User::create($newUser)]);
+    }
+
+    public function update(UserPutRequest $request, User $user)
+    {
+        if ($user->type == 'C')
+            return response(['message' => 'You are not authorized to update client users'], 403);
+
+        $newUser = $request->validated();
+
+        if ($request->hasFile('photo')) {
+            $newUser['photo_url'] = basename($request->file('photo')->store('public/fotos'));
+            unset($newUser['photo']);
+
+            //Delete previous photo
+            $newUser->photo_url ? Storage::delete('public/fotos/' . $newUser->photo_url) : null;
+        }
+
+        $user->update($newUser);
+        return response(['message' => 'User updated']);
     }
 }
