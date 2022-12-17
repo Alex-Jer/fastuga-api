2ec8992d88b184f9da4b424fc2aa4dc8a979e666
tgn <trwygon@outlook.pt>
2022-12-17 23:22:02 +0000

    Create OrderPostRequest.php

---
+++ app/Http/Requests/Order/OrderPostRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'customer' => 'nullable|numeric|exists:customers,id|required_with:points_used',
+            'payment_type' => 'required|string|in:VISA,PAYPAL,MBWAY',
+            'payment_reference' => 'required|string',
+            'points_used' => 'nullable|numeric|min:0',
+            'cart' => 'required|array:id,quantity,note',
+        ];
+    }
+}
