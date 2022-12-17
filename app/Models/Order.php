d6f344b1712c8d206fb51261ba2558de66f78600
tgn <trwygon@outlook.pt>
2022-12-17 22:21:36 +0000

    update order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,6 +10,34 @@ class Order extends Model
     use HasFactory;
     protected $dates = ['updated_at', 'created_at'];
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'ticket_number',
+        'status',
+        'customer_id',
+        'total_price',
+        'total_paid',
+        'total_paid_with_points',
+        'points_gained',
+        'points_used_to_pay',
+        'payment_type',
+        'payment_reference',
+        'date'
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'date' => 'datetime'
+    ];
+
     public function customer()
     {
         return $this->belongsTo(Customer::class, 'customer_id');
