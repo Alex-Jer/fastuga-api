23852cfc4e9e12bbb0e28ec6053735f4d8740e13
tgn <trwygon@outlook.pt>
2022-12-19 17:22:43 +0000

    removed email verification comments from routes

--- routes/api.php
+++ routes/api.php
@@ -28,32 +28,12 @@ Route::controller(AuthController::class)->group(function () {
     //Route::get('scopes', 'scopes')->middleware('auth:api');
 });
 
-Route::prefix('users/me')->group(function () {
-    /*Route::get('/email/verify', function () {
-        //  Only used when a logged in user tries to access a "verified" middleware protected route
-        // (currently unused)
-        return response(['message' => 'User\'s email is not verified. Cannot complete requested action.'], 403);
-    })->name('verification.notice');*/
-
-    /*Route::get('/forgot-password', 'forgotPassword')->name('password.email');
-    Route::post('/reset-password/{token}', function ($token) {
-        return view('auth.reset-password', ['token' => $token]);
-    })->name('password.reset');*/
-});
-
 Route::prefix('users')->controller(UserController::class)->middleware('auth:api')->group(function () {
     Route::prefix('me')->group(function () {
         Route::get('/', 'showMe');
         Route::put('/', 'updateMe')->name('update-employee-profile');
         Route::patch('/password', 'changePassword');
         Route::patch('/email', 'changeEmail');
-
-        /*Route::patch('/email/verify', 'verifyMyEmail')->name('verification.send');
-        Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-            $request->fulfill();
-
-            return response(['message' => 'Your email was successfully verified'], 200);
-        })->middleware('signed')->name('verification.verify');*/
     });
 
 
