abbeb8975b9f58f40e65db52bf6173cb6c7cab83
tgn <trwygon@outlook.pt>
2022-12-21 22:43:31 +0000

    next test

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -14,12 +14,13 @@ class OrderResource extends JsonResource
      */
     public function toArray($request)
     {
+        $usr = $this->customer()->withTrashed()->get()[0]->user()->withTrashed()->get()[0];
         return array_merge([
             'id' => $this->id,
             'ticket_number' => $this->ticket_number,
             'status' => $this->status,
-            'customer_name' => ($this->customer ? $this->customer()->withTrashed()->get()[0]->user()->withTrashed()->get()[0]->name : null),
-            'customer_photo_url' => ($this->customer ? $this->customer->user->photo_url : null),
+            'customer_name' => ($this->customer_id ? $usr->name : null),
+            'customer_photo_url' => ($this->customer_id ? $usr->photo_url : null),
             'total_price' => $this->total_price,
             'total_paid' => $this->total_paid,
             'total_paid_with_points' => $this->total_paid_with_points,
